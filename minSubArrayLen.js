d6112f6fda923cdc386d6ca0b7fb014874200995
Oke Uguru <ondukwe@gmail.com>
2019-08-22 18:49:34 -0500

    updated code

--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -5,6 +5,8 @@
 // of which the sum is greater than or equal to the integer passed to the
 // function. If there is one, return 0 instead
 
+function minSubArray(arr, num) {}
+
 minSubArray([2, 3, 1, 2, 4, 3], 7);
 minSubArray([2, 1, 6, 5, 4], 9);
 minSubArray([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52);
