709ee6fe135ea89a48e1b22cbca3adee89a8a287
Oke Uguru <ondukwe@gmail.com>
2019-08-27 10:32:56 -0400

    Updated isSubsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -4,6 +4,18 @@
 // should check whether the characters in the first string appear
 // somewhere in the second string, without their order changing.
 
+function isSubsequence(str1, str2) {
+  if (str1.length < 1 || str2.length < 1) {
+    return null;
+  }
+
+  if (str2.includes(str1)) {
+    console.log(`${str1} is contained in ${str2}`);
+  } else {
+    console.log(`${str1} is NOT contained in ${str2}`);
+  }
+}
+
 isSubsequence("hello", "hello world");
 isSubsequence("sing", "sting");
 isSubsequence("abc", "abracadabra");
