fb966c69a623f3a11c68d3c603721d1be696c1c5
Oke Uguru <ondukwe@gmail.com>
2019-09-04 13:40:56 -0400

    updated findLongestSubstring.js

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -2,21 +2,25 @@
 // returns the length of the longest substring with all distinct characters
 
 function findLongestSubstring(str) {
-  let longest = 0;
-  let seen = {};
   let start = 0;
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (seen[char]) {
-      start = Math.max(start, seen[char]);
-    }
-    // index - beginni ng of substring + 1 (to include current in count)
-    longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
-    seen[char] = i + 1;
-  }
-  return longest;
 }
+
+// function findLongestSubstring(str) {
+//   let longest = 0;
+//   let seen = {};
+//   let start = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (seen[char]) {
+//       start = Math.max(start, seen[char]);
+//     }
+//     // index - beginning of substring + 1 (to include current in count)
+//     longest = Math.max(longest, i - start + 1);
+//     // store the index of the next char so as to not double count
+//     seen[char] = i + 1;
+//   }
+//   return longest;
+// }
 
 findLongestSubstring("");
 findLongestSubstring("rithmschool");
